bafcd15e6d3cbea6a2326dda2f475ab5295f1004
JohnyP <81161435+JohnyP36@users.noreply.github.com>
2022-03-10 11:07:53 +0100

    Update octokit_client.rb

--- connect4/octokit_client.rb
+++ connect4/octokit_client.rb
@@ -42,7 +42,7 @@ class OctokitClient
     @octokit.update_contents(@repository, filepath, message, sha, content)
   end
   
-  def issues(labels: 'documentation')
+  def issues(labels: '')
     @issues ||= @octokit.list_issues(
       @repository,
       state: 'closed',
