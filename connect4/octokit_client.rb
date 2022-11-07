623553e76da4f00644f0a95d0f340fa384aaa578
JohnyP <81161435+JohnyP36@users.noreply.github.com>
2022-11-07 18:35:35 +0000

    Update client

--- connect4/octokit_client.rb
+++ connect4/octokit_client.rb
@@ -1,21 +1,16 @@
+nes (48 sloc) 1.45 KB
 require 'octokit'
 
 class OctokitClient
-  PREVIEW_HEADERS = [
-    ::Octokit::Preview::PREVIEW_TYPES[:reactions],
-    ::Octokit::Preview::PREVIEW_TYPES[:integrations]
-  ].freeze
-
   def initialize(github_token:, repository:, issue_number:)
     @octokit = Octokit::Client.new(access_token: github_token)
     @octokit.auto_paginate = true
-    @octokit.default_media_type = ::Octokit::Preview::PREVIEW_TYPES[:integrations]
     @repository = repository
     @issue_number = issue_number
   end
 
   def add_reaction(reaction:)
-    @octokit.create_issue_reaction(@repository, @issue_number, reaction, {accept: PREVIEW_HEADERS})
+    @octokit.create_issue_reaction(@repository, @issue_number, reaction)
   end
 
   def add_comment(comment:)
@@ -25,7 +20,7 @@ class OctokitClient
   def add_label(label:)
     @octokit.add_labels_to_an_issue(@repository, @issue_number, [label])
   end
-  
+
   def close_issue
     @octokit.close_issue(@repository, @issue_number)
   end
@@ -41,14 +36,13 @@ class OctokitClient
   def write_to_repo(filepath:, message:, sha:, content:)
     @octokit.update_contents(@repository, filepath, message, sha, content)
   end
-  
+
   def issues(labels: '')
     @issues ||= @octokit.list_issues(
       @repository,
       state: 'closed',
       labels: labels,
-      accept: PREVIEW_HEADERS
-    )&.select{ |issue| issue.reactions.confused == 0 }
+    )&.select { |issue| issue.reactions.confused.zero? }
   end
 
   def error_notification(reaction:, comment:, error: nil)
